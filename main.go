be62cc4959f1fd073dc3bebcd47c22cb5a3dd540
Ronan Martin <ronanmartin86@outlook.com>
2022-09-19 15:41:04 -0300

    Incluso primeiro modelo de código para Hash-Table

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Pessoa struct {
 	Nome      string
@@ -19,7 +22,7 @@ func main() {
 	}
 
 	table := HashTable{}
-	
+
 	keys := make([]int, len(pessoas))
 	for i, pessoa := range pessoas {
 		keys[i] = table.Put(pessoa)
@@ -51,4 +54,48 @@ func main() {
 	ronan := table.Search("Ronan")
 	fmt.Println(ronan)
 
+}
+
+type HashTable struct {
+	items map[int][]Pessoa
+	lock  sync.RWMutex
+}
+
+func hash(nome string) (key int) {
+	for _, letra := range nome {
+		key = 31*key + int(letra)
+	}
+
+	return
+}
+
+func (ht *HashTable) Put(pessoa Pessoa) int {
+	ht.lock.Lock()
+	defer ht.lock.Unlock()
+	key := hash(pessoa.Nome)
+	if ht.items == nil {
+		ht.items = make(map[int][]Pessoa)
+	}
+	ht.items[key] = append(ht.items[key], pessoa)
+	return key
+}
+
+func (ht *HashTable) Remove(nome string) {
+	ht.lock.Lock()
+	defer ht.lock.Unlock()
+	key := hash(nome)
+	delete(ht.items, key)
+}
+
+func (ht *HashTable) Get(key int) []Pessoa {
+	ht.lock.RLock()
+	defer ht.lock.RUnlock()
+	return ht.items[key]
+}
+
+func (ht *HashTable) Search(nome string) []Pessoa {
+	ht.lock.RLock()
+	defer ht.lock.RUnlock()
+	key := hash(nome)
+	return ht.items[key]
 }
