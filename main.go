81910172ffc73cd1d055386549b2db243dab05a9
RonanMartin <https://github.com/RonanMartin>
2022-09-20 15:37:58 -0300

    Separado código em dois arquivos

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
+
+// Como está em mais de um arquivo, para funcionar é necessário fazer o comando: go run .
+// Desta forma Go vai ler todos os arquivos da pasta e pegar também o arquivo hash_table.go
 
 type Pessoa struct {
 	Nome      string
@@ -54,48 +56,4 @@ func main() {
 	ronan := table.Search("Ronan")
 	fmt.Println(ronan)
 
-}
-
-type HashTable struct {
-	items map[int][]Pessoa
-	lock  sync.RWMutex
-}
-
-func hash(nome string) (key int) {
-	for _, letra := range nome {
-		key = 31*key + int(letra)
-	}
-
-	return
-}
-
-func (ht *HashTable) Put(pessoa Pessoa) int {
-	ht.lock.Lock()
-	defer ht.lock.Unlock()
-	key := hash(pessoa.Nome)
-	if ht.items == nil {
-		ht.items = make(map[int][]Pessoa)
-	}
-	ht.items[key] = append(ht.items[key], pessoa)
-	return key
-}
-
-func (ht *HashTable) Remove(nome string) {
-	ht.lock.Lock()
-	defer ht.lock.Unlock()
-	key := hash(nome)
-	delete(ht.items, key)
-}
-
-func (ht *HashTable) Get(key int) []Pessoa {
-	ht.lock.RLock()
-	defer ht.lock.RUnlock()
-	return ht.items[key]
-}
-
-func (ht *HashTable) Search(nome string) []Pessoa {
-	ht.lock.RLock()
-	defer ht.lock.RUnlock()
-	key := hash(nome)
-	return ht.items[key]
 }
